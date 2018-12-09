ff4993b1633ace32143a4aaea1bcd20fa9a81089
Julien Lafourcade <julien.lafourcade@gmail.com>
2018-12-09 11:44:39 +0100

    Supression du filtre qui ne fonctionne pas (à revoir plus tard)

--- asciidoctor-revealjs.js
+++ asciidoctor-revealjs.js
@@ -7,7 +7,5 @@ asciidoctor.convertFile('jsp.adoc', options);
 
 // Copie des assets nécessaires
 const fs = require('fs-extra')
-fs.copySync('node_modules/reveal.js', 'output/node_modules/reveal.js', { 'filter' : (p) => {    
-    return (p.indexOf('node_modules/reveal.js/css') > -1) || (p.indexOf('node_modules/reveal.js/js') > -1) || (p.indexOf('node_modules/reveal.js/lib') > -1)
-}})
+fs.copySync('node_modules/reveal.js', 'output/node_modules/reveal.js')
 
