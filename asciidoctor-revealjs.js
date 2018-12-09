a2f315ddd251e65c24bcade6f982d9510a8771e4
Julien Lafourcade <julien.lafourcade@gmail.com>
2018-12-09 11:39:36 +0100

    Copie des assets reveal

--- asciidoctor-revealjs.js
+++ asciidoctor-revealjs.js
@@ -4,3 +4,10 @@ require('asciidoctor-reveal.js');
 var attributes = {'revealjsdir': 'node_modules/reveal.js@'};
 var options = {safe: 'safe', backend: 'revealjs', attributes: attributes, to_dir: 'output', mkdirs: true};
 asciidoctor.convertFile('jsp.adoc', options); 
+
+// Copie des assets nécessaires
+const fs = require('fs-extra')
+fs.copySync('node_modules/reveal.js', 'output/node_modules/reveal.js', { 'filter' : (p) => {    
+    return (p.indexOf('node_modules/reveal.js/css') > -1) || (p.indexOf('node_modules/reveal.js/js') > -1) || (p.indexOf('node_modules/reveal.js/lib') > -1)
+}})
+
