db351bd14efc78f261a4e0cd92867b35a3d08770
Julien LAFOURCADE <julien.lafourcade@asso-cocktail.fr>
2018-12-10 16:05:53 +0100

    Ajout d'un watch pour la recompilation

--- asciidoctor-revealjs.js
+++ asciidoctor-revealjs.js
@@ -1,7 +1,7 @@
 var asciidoctor = require('asciidoctor.js')();
 require('asciidoctor-reveal.js');
 
-var attributes = {'revealjsdir': 'node_modules/reveal.js@'};
+var attributes = {'revealjsdir': 'node_modules/reveal.js@', 'revealjs_history': true};
 var options = {safe: 'safe', backend: 'revealjs', attributes: attributes, to_dir: 'output', mkdirs: true};
 asciidoctor.convertFile('jsp.adoc', options); 
 
