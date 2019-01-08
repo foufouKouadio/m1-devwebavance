cd3586b6b67a0d90dcf735417e8adb2ff2812ab1
Julien Lafourcade <julien.lafourcade@gmail.com>
2019-01-08 08:04:59 +0100

    [TECH] Ajout de PlantUML

--- asciidoctor-revealjs.js
+++ asciidoctor-revealjs.js
@@ -1,9 +1,19 @@
 var asciidoctor = require('asciidoctor.js')();
 require('asciidoctor-reveal.js');
+const plantuml = require('asciidoctor-plantuml');
+plantuml.register(asciidoctor.Extensions);
 
-var attributes = {'revealjsdir': 'node_modules/reveal.js@', 'revealjs_history': true};
+var attributes = {
+    'revealjsdir': 'node_modules/reveal.js@', 
+    'revealjs_history': true,
+    'revealjs_theme': 'black',
+    'revealjs_width': 1280,
+    'revealjs_height': 720
+};
 var options = {safe: 'safe', backend: 'revealjs', attributes: attributes, to_dir: 'output', mkdirs: true};
 asciidoctor.convertFile('jsp.adoc', options); 
+asciidoctor.convertFile('injection-dependances.adoc', options); 
+asciidoctor.convertFile('rest.adoc', options); 
 
 // Copie des assets nécessaires
 const fs = require('fs-extra')
