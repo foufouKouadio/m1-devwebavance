2c40dc85b4d3f4430524a84a9323e17423f2b41a
Julien Lafourcade <julien.lafourcade@gmail.com>
2019-01-21 06:42:12 +0100

    Rest OK et ajout des docs Micronaut

--- asciidoctor-revealjs.js
+++ asciidoctor-revealjs.js
@@ -8,12 +8,15 @@ var attributes = {
     'revealjs_history': true,
     'revealjs_theme': 'black',
     'revealjs_width': 1280,
-    'revealjs_height': 720
+    'revealjs_height': 720,
+    'revealjs_slideNumber': true
 };
 var options = {safe: 'safe', backend: 'revealjs', attributes: attributes, to_dir: 'output', mkdirs: true};
 asciidoctor.convertFile('jsp.adoc', options); 
 asciidoctor.convertFile('injection-dependances.adoc', options); 
 asciidoctor.convertFile('rest.adoc', options); 
+asciidoctor.convertFile('micronaut-intro.adoc', options); 
+asciidoctor.convertFile('micronaut-rest.adoc', options); 
 
 // Copie des assets nécessaires
 const fs = require('fs-extra')
